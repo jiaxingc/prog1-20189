d8d0e36e8b700aee064ab02920d5bf4e64f1fb25
jiaxingc <43052057+jiaxingc@users.noreply.github.com>
2018-09-06 21:03:27 -0300

    Add files via upload

---
+++ grau celsius para fahreheit.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    float c,f;
+    printf("digite o grau celsius:");
+    scanf("%f",&c);
+    f=1.8*c+32;
+    printf("o grau fahrenheit e =%.2f\n",f);
+    system("pause");
+    return 0;
+}
+    
